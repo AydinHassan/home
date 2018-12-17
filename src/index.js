165a50055b45669f237cdb71ed5953dd2e3a8b43
Aydin Hassan <aydin@hotmail.co.uk>
2018-12-17 13:48:59 +0400

    Open source section

--- src/index.js
+++ src/index.js
@@ -7,8 +7,28 @@ import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
 import { faCalendarDay } from "@fortawesome/free-solid-svg-icons/faCalendarDay";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons/faChevronDown";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons/faCodeBranch";
 
 config.autoAddCss = false;
 
-library.add(faTwitter, faGithub, faLinkedinIn, faInstagram, faCalendarDay, faChevronDown);
+library.add(faTwitter, faGithub, faLinkedinIn, faInstagram, faCalendarDay, faChevronDown, faCodeBranch);
 dom.watch();
+
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
+
+for(let el of tabLinks) {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector('.tabs li.active').classList.remove("active");
+        document.querySelector('.tabs-panel.active').classList.remove("active");
+
+        const parentListItem = el.parentElement;
+
+        parentListItem.classList.add("active");
+        const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+        const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+        panel[0].classList.add("active");
+    });
+}
